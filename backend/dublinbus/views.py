454b538326ea46bc9ac55512519cd6994df9f48f
AndrewJohnHughes <andrew.hughes1@ucdconnect.ie>
2021-06-24 10:32:50 +0100

    DUB-19 Stops route

--- backend/dublinbus/views.py
+++ backend/dublinbus/views.py
@@ -11,6 +11,23 @@ def index(request):
     """Temporary homepage for the application"""
     return render(request, 'dublinbus/index.html')
 
+def stops(request):
+    """Returns a list of dictionaries of all the bus stops in Dublin Bus."""
+
+    result = []
+    distinct_stop_ids = Stop.objects.values_list('stop_id', flat=True) # .distinct()
+    for stop_id in distinct_stop_ids:
+    
+        stop_details = Stop.objects.get(stop_id=stop_id)
+        result.append({
+                'stop_name': stop_details.stop_name, # .split(",")[0],
+                'stop_number': stop_details.stop_name.split("stop ")[-1],
+                'stop_lat': stop_details.stop_lat,
+                'stop_lon': stop_details.stop_lon
+                })
+
+    return JsonResponse(result, safe=False)
+
 def stop(request, stop_id):
     """Returns all of the scheduled arrivals for a particular stop within the next hour and
     any delays to those schedules from the real-time data."""
@@ -40,17 +57,20 @@ def stop(request, stop_id):
     realtime_updates = request_realtime_nta_data()
 
     for stop_time in stop_time_details:
+
+        delay = get_realtime_dublin_bus_delay(realtime_updates,
+                                              stop_time.trip.trip_id,
+                                              stop_time.stop_sequence)
+
         result['arrivals'].append({
             'route_id': stop_time.trip.route.route_id,
             'trip_id': stop_time.trip.trip_id,
             'scheduled_arrival_time': stop_time.arrival_time,
             'scheduled_departure_time': stop_time.departure_time,
             'stop_sequence': stop_time.stop_sequence,
-            'delay': get_realtime_dublin_bus_delay(
-                realtime_updates,
-                stop_time.trip.trip_id,
-                stop_time.stop_sequence
-            )
+            'delay_sec': delay,
+            #'due_in_sec': get_due_in_time(current_time, stop_time.arrival_time, delay, "sec"),
+            'due_in_min': get_due_in_time(current_time, stop_time.arrival_time, delay, "min")
         })
 
     return JsonResponse(result)
@@ -105,7 +125,6 @@ def get_realtime_dublin_bus_delay(realtime_updates, trip_id, stop_sequence):
     """
     Get the current delay for a particular trip from the response from the
     realtime NTA API.
-
     Args
     ---
         realtime_updates: list
@@ -114,7 +133,7 @@ def get_realtime_dublin_bus_delay(realtime_updates, trip_id, stop_sequence):
             The ID for the trip we want to get the delay for
         stop_sequence: int
             The stop sequence number for this stop in this trip
-
+            
     Returns
     ---
         The current delay for the trip in seconds. 0 means there is no delay and a negative
@@ -143,3 +162,39 @@ def get_realtime_dublin_bus_delay(realtime_updates, trip_id, stop_sequence):
                 return stop_time_updates[0]['departure']['delay']
 
     return 0
+
+
+def get_due_in_time(current_time, scheduled_arrival_time, delay, unit_time):
+    """
+    Calculate the expected due time from now for a particular trip.
+
+    Args
+    ---
+        current_time: datetime object
+            The time of when query was made in UTC.
+        scheduled_arrival_time: datetime.time
+            The time the bus is scheduled to arrive at the stopid in UTC.
+        delay: int
+            The current delay for the trip in seconds.
+        unit_time: str
+            Either "sec" or "min" to specify the unit of time the function is to return the due time in.
+    Returns
+    ---
+        The expected due time for trip in seconds or minutes as an int.
+    """
+
+    # extract date from curr_time and concatenate to scheduled_arrival_time (time past midnight) e.g. "23/06/21 " + "12:04:25" = "23/06/21 12:04:25"
+    scheduled_arrival_datetime_str = current_time.strftime("%d/%m/%y ") + str(scheduled_arrival_time)
+    # create datetime object for scheduled_arrival_datetime
+    scheduled_arrival_datetime_obj = datetime.strptime(scheduled_arrival_datetime_str, '%d/%m/%y %H:%M:%S').replace(
+        tzinfo=timezone(timedelta(hours=1)))  # tzinfo=timezone.utc
+    # subtract current_time from scheduled_arrival_datetime_obj
+    # scheduled_arrival_time given to second precision so removing microsecond precision from current_time
+    time_delta = scheduled_arrival_datetime_obj - current_time.replace(microsecond=0)
+    # add delay to due time
+    time_delta_seconds = time_delta.total_seconds() + delay
+
+    if unit_time == "sec":
+        return int(time_delta_seconds)
+    elif unit_time == "min":
+        return round(time_delta_seconds / 60)  # important decision for display - round(), math.floor() or math.ceil()
