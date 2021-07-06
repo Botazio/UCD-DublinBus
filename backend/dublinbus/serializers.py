f5157a752c531e6cc4349144d8cdbfaecc9fbc45
AndrewJohnHughes <andrew.hughes1@ucdconnect.ie>
2021-07-06 13:11:20 +0100

    fixing pylint issues

--- backend/dublinbus/serializers.py
+++ backend/dublinbus/serializers.py
@@ -19,7 +19,7 @@ class UserSerializerWithToken(serializers.ModelSerializer):
     token = serializers.SerializerMethodField()
     password = serializers.CharField(write_only=True)
 
-    def get_token(obj):
+    def get_token(self, obj):
         ''' Method which handles the manual creation of a new token '''
         jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
         jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
