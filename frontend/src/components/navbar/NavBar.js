38af0452b9451f9312834aa2b131d49d8de4e6e9
Alvaro Garcia <alvaro.garcia@ucdconnect.ie>
2021-07-09 13:24:53 +0200

    Keep trying the linter

--- frontend/src/components/navbar/NavBar.js
+++ frontend/src/components/navbar/NavBar.js
@@ -6,29 +6,31 @@ import PopoverUser from '../popover-user/PopoverUser';
 import { useEffect } from 'react';
 
 const Navbar = () => {
-  // change the user icon if there is a current user
-  const { currentUser, logout, isAuthenticated } = useAuth();
+   // change the user icon if there is a current user
+   const { currentUser, logout, isAuthenticated } = useAuth();
 
-  useEffect(() => {
-    isAuthenticated();
-    // eslint-disable-next-line
-  }, [])
+   useEffect(() => {
+      isAuthenticated();
+      // eslint-disable-next-line
+   }, [])
 
-  return (
-    <nav className={NavbarCSS.navbar}>
-      {/* We use this div to insert the list icon */}
-      <div id="list_icon"></div>
-      <div className={NavbarCSS.navbar_logo}>
-        <h2 className={NavbarCSS.logo_navbar_phone} id="logo_navbar_phone"> </h2>
-        <h2 className={NavbarCSS.logo_navbar_desktop} id="logo_navbar_desktop">DUBLIN BUS</h2>
-      </div>
-      <div className={NavbarCSS.login_icon}>
-        {/* Change the icon logo if there is a user log in*/}
-        {!currentUser && <Link to='/login'><Login height={'25'} /></Link>}
-        {currentUser && <PopoverUser currentUser={currentUser} logout={logout} />}
-      </div>
-    </nav>
-  );
+   const hello = "hey"
+
+   return (
+      <nav className={NavbarCSS.navbar}>
+         {/* We use this div to insert the list icon */}
+         <div id="list_icon"></div>
+         <div className={NavbarCSS.navbar_logo}>
+            <h2 className={NavbarCSS.logo_navbar_phone} id="logo_navbar_phone"> </h2>
+            <h2 className={NavbarCSS.logo_navbar_desktop} id="logo_navbar_desktop">DUBLIN BUS</h2>
+         </div>
+         <div className={NavbarCSS.login_icon}>
+            {/* Change the icon logo if there is a user log in*/}
+            {!currentUser && <Link to='/login'><Login height={'25'} /></Link>}
+            {currentUser && <PopoverUser currentUser={currentUser} logout={logout} />}
+         </div>
+      </nav>
+   );
 }
 
 export default Navbar;
