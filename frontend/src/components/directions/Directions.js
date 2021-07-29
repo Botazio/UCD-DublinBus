a965e5309dd1ac6079de80dfa80fbcd5c2116129
Alvaro Garcia <alvaro.garcia@ucdconnect.ie>
2021-07-29 11:47:28 +0200

    Creation of line search bar

--- frontend/src/components/directions/Directions.js
+++ frontend/src/components/directions/Directions.js
@@ -3,9 +3,12 @@ import DirectionsSearcher from "./subcomponents/DirectionsSearcher";
 // This component is the main component for the directions section.
 // The subcomponents are called from this component
 const Directions = () => {
+  // This state controls if a line has been selected before displaying
+  // the search bars
+  const [activeLine, setActiveLine] = useState(null);
   return (
     <>
-      <DirectionsSearcher />
+      {activeLine && <DirectionsSearcher />}
     </>
   );
 };
