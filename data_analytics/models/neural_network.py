49f3242b14f5d222abe069a64a140bb24d383e78
Paul Reidy <paul-reidy@outlook.com>
2021-08-04 21:31:01 +0100

    DUB-101 Basic paralellisation of models (#53)

--- data_analytics/models/neural_network.py
+++ data_analytics/models/neural_network.py
@@ -1,4 +1,3 @@
-import datetime
 import argparse
 
 from .utils import train_all_stop_pair_models
@@ -6,11 +5,16 @@ from .utils import train_all_stop_pair_models
 # Command line argument to turn on downloading of new data
 parser = argparse.ArgumentParser()
 parser.add_argument('--grid_search', help="Perform Grid Search", action="store_true")
+parser.add_argument('--start_num', type=int)
+parser.add_argument('--end_num', type=int)
 args = parser.parse_args()
-
-current_time = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
 
 if args.grid_search:
     train_all_stop_pair_models("NeuralNetwork", grid_search=True)
 else:
-    train_all_stop_pair_models("NeuralNetwork", grid_search=False)
+    train_all_stop_pair_models(
+        "NeuralNetwork",
+        grid_search=False,
+        start_num=0 if not args.start_num else args.start_num,
+        end_num=args.end_num
+    )
