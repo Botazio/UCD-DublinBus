7413bc5fc807c2aedc5ae607a2c81ac70f226332
AndrewJohnHughes <andrew.hughes1@ucdconnect.ie>
2021-08-12 01:13:44 +0100

    DUB-105 Google OAuth Token used too early

--- backend/dublinbus/providers.py
+++ backend/dublinbus/providers.py
@@ -1,6 +1,38 @@
+from google.auth import jwt
+from google.auth import _helpers
 from google.auth.transport import requests
 from google.oauth2 import id_token
 import facebook
+
+# overriding _verify_iat_and_exp to account for server system clock 
+# being behind by approximately 25 minutes
+def _verify_iat_and_exp_edited(payload):
+    """Verifies the ``iat`` (Issued At) and ``exp`` (Expires) claims in a token
+    payload.
+
+    Args:
+        payload (Mapping[str, str]): The JWT payload.
+
+    Raises:
+        ValueError: if any checks failed.
+    """
+    now = _helpers.datetime_to_secs(_helpers.utcnow())
+
+    # Make sure the iat and exp claims are present.
+    for key in ("iat", "exp"):
+        if key not in payload:
+            raise ValueError("Token does not contain required claim {}".format(key))
+
+    # Make sure the token wasn't issued in the future.
+    iat = payload["iat"]
+    # Err on the side of accepting a token that is slightly early to account
+    # for clock skew.
+    earliest = iat - _helpers.CLOCK_SKEW_SECS  - 1800
+    if now < earliest:
+        raise ValueError("Token used too early - even on ucd server!, {} < {}".format(now, iat))
+
+jwt._verify_iat_and_exp = _verify_iat_and_exp_edited
+
 
 class Provider:
     """Class to fetch the user info from Google or Facebook and return it"""
