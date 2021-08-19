0a0a30fc896f4091457a6e4ff3bcc4ffb5910a05
Alvaro Garcia <alvaro.garcia@ucdconnect.ie>
2021-08-19 13:43:00 +0200

    Finished implementing backend routes and markers

--- frontend/src/components/map-container/MapContainer.js
+++ frontend/src/components/map-container/MapContainer.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { GoogleMap, useLoadScript } from "@react-google-maps/api";
 import MapContainerCSS from "./MapContainer.module.css";
 import lightTheme from "../../fixtures/map-styles/lightTheme";
@@ -9,6 +9,8 @@ import { useTheme } from "@material-ui/core";
 import { useLines } from "../../providers/LinesContext";
 import CustomError from "../../reusable-components/error/CustomError";
 import Waiting from "../../reusable-components/waiting/Waiting";
+import { useAuth } from "../../providers/AuthContext";
+import { useEffect } from "react";
 
 // Google map libraries
 const libraries = ["places"];
@@ -20,12 +22,21 @@ const containerStyle = {
   height: "100%",
 };
 
-// Options for the map.
+// Custom options for the map.
 const options = {
   mapTypeControl: false,
   fullscreenControl: true,
   streetViewControl: false,
 };
+
+// Default options for the map
+const defaultOptions = {
+  mapTypeControl: false,
+  fullscreenControl: true,
+  streetViewControl: false,
+  styles: lightTheme,
+};
+
 
 // dict for the mapStyles
 const mapStyles = {
@@ -43,18 +54,15 @@ const center = {
 // This is the main component for the bus page. 
 // Loads a google map and wraps all the other components in a context
 export default function MapContainer(props) {
+  // State to control the map theme
+  const [theme, setTheme] = useState();
+
   // Loads the map
   const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: "AIzaSyCLGkrQoHNmMyhdvwl0wmron1ndtREM0zM",
     libraries,
     version: "3.46",
   });
-
-  // mapRef to use in other parts of the code without triggering rerenders
-  const mapRef = React.useRef();
-  const onMapLoad = React.useCallback((map) => {
-    mapRef.current = map;
-  }, []);
 
   // Call the providers to load the data for every component
   // It is not a problem to call it multiple times because it only performs the fetch operation
@@ -63,9 +71,15 @@ export default function MapContainer(props) {
   useStops();
   useLines();
 
-  // Render a different map depending on the user settings
-  const mapTheme = mapStyles[useTheme().map];
-  options.styles = mapTheme;
+  const { currentUser } = useAuth();
+  useEffect(() => {
+    // If the user is authenticated set the map to the user preferences
+    if (currentUser) {
+      handleTheme(mapStyles[currentUser.map]);
+    }
+
+  }, [currentUser]);
+
 
   // Error handling when loading the map
   if (loadError) return <CustomError message="Error loading maps" height="60" />;
@@ -73,18 +87,58 @@ export default function MapContainer(props) {
 
   return (
     <>
-      <div className={MapContainerCSS.map_container}>
+      {/* If there is no user render a default map */}
+      {!currentUser && <div className={MapContainerCSS.map_container}>
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={center}
+          zoom={13}
+          options={defaultOptions}
+        >
+          {/* GoogleMap component works as a context around the children */}
+          {props.children}
+        </GoogleMap>
+      </div>}
+
+      {/* If there is a user render a map with his preferences */}
+      {currentUser && theme && <div className={MapContainerCSS.map_container}>
         <GoogleMap
           mapContainerStyle={containerStyle}
           center={center}
           zoom={13}
           options={options}
-          onLoad={onMapLoad}
         >
           {/* GoogleMap component works as a context around the children */}
           {props.children}
         </GoogleMap>
-      </div>
+      </div>}
+
     </>
   );
+
+  // Function to handle the map theme adding google markers to it
+  function handleTheme(mapTheme) {
+    // Delete the property owner as it refers to the user pk
+    delete currentUser.markers.owner;
+
+    const newTheme = [...mapTheme];
+
+    // Loop through the object properties
+    for (const key of Object.keys(currentUser.markers)) {
+      // Add the markers to the map if active
+      if (currentUser.markers[key]) {
+        newTheme.push({
+          featureType: `poi.${key}`,
+          elementType: "all",
+          stylers: [
+            {
+              visibility: "on",
+            },
+          ]
+        });
+      }
+    }
+    setTheme(newTheme);
+    options.styles = newTheme;
+  }
 }
