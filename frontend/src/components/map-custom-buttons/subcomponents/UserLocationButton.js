7eefb76960cae85e95e61810f3c434ee058c85d0
Alvaro Garcia <alvaro.garcia@ucdconnect.ie>
2021-08-21 00:21:21 +0200

    Fixed map button so it does not display a marker when panning to the user position

--- frontend/src/components/map-custom-buttons/subcomponents/UserLocationButton.js
+++ frontend/src/components/map-custom-buttons/subcomponents/UserLocationButton.js
@@ -1,18 +1,22 @@
 import MapCustomButtonsCSS from "../MapCustomButtons.module.css";
 import iconPosition from "../fixtures/icon-position.png";
-import { useState } from "react";
-import CustomMarker from "../../../reusable-components/custom-marker/CustomMarker";
-import iconBlueDot from "../../../fixtures/icons/icon-bluedot.png";
+import { useGeolocation } from "../../../providers/GeolocationContext";
 
 // Displays a marker at the user location when it is clicked
 const UserLocationButton = ({ mapRef }) => {
-  const [position, setPosition] = useState(false);
+
+  const { position, error } = useGeolocation();
+
+  const panToUserPosition = () => {
+    if (position) mapRef.panTo(position);
+    if (error) alert("User denied Geolocation");
+  };
 
   return (
     <>
       <div
         className={MapCustomButtonsCSS.button + " " + MapCustomButtonsCSS.button_bottom}
-        onClick={() => getUserPosition()}
+        onClick={() => panToUserPosition()}
       >
         <img
           src={iconPosition}
@@ -20,45 +24,9 @@ const UserLocationButton = ({ mapRef }) => {
           alt={"icon position"}
         />
       </div>
-
-      {/* Display a marker if we get the position back */}
-      {position && (
-        <CustomMarker
-          id="user-position"
-          position={position}
-          options={{
-            icon: {
-              url: iconBlueDot,
-              origin: new window.google.maps.Point(0, 0),
-              anchor: new window.google.maps.Point(10, 10),
-            }
-          }}
-        />
-      )}
     </>
   );
 
-  function getUserPosition() {
-    if (!navigator.geolocation) {
-      alert("Geolocation is not supported by your browser");
-      return;
-    }
-
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        setPosition({
-          lat: position.coords.latitude,
-          lng: position.coords.longitude,
-        });
-        mapRef.setZoom(13);
-        mapRef.panTo({
-          lat: position.coords.latitude,
-          lng: position.coords.longitude,
-        });
-      },
-      (err) => alert(err.message)
-    );
-  }
 };
 
 export default UserLocationButton;
