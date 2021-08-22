37a0e625a427296e42394c8298b1becb060769ae
AndrewJohnHughes <andrew.hughes1@ucdconnect.ie>
2021-08-22 15:31:49 +0100

    unittest failed - ENGINE

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -139,7 +139,8 @@ else:
             'HOST': 'dublin-bus.caghf9c2wznv.eu-west-1.rds.amazonaws.com',
             'PORT': '3306',
             'TEST': {
-                'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': BASE_DIR / 'db.sqlite3',
                 },
         }
     }
